3accc6657ca64e89d015f86741cdd628ce45d89f
Yasser4440 <157912825+Yasser444o@users.noreply.github.com>
2025-12-10 19:10:20 +0200

    v1.21.16 snapshot 2
    - Added a new Optional QoL Addon: "Sawdust"
    - It helps to chop trees faster by moving the highest log in the tree to your axe
    - Added a new utility to the Creative tool to make you break multiple blocks of the same type by just breaking one block

---
+++ skybedrock_addons/sawdust/scripts/sawdust.js
@@ -0,0 +1,63 @@
+import { world, system } from "@minecraft/server"
+
+const logs = {
+	'minecraft:oak_log': ['minecraft:oak_leaves', 'minecraft:azalea_leaves', 'minecraft:azalea_leaves_flowered', ],
+	'minecraft:spruce_log' : 'minecraft:spruce_leaves',
+	'minecraft:birch_log': 'minecraft:birch_leaves',
+	'minecraft:jungle_log': 'minecraft:jungle_leaves',
+	'minecraft:dark_oak_log': 'minecraft:dark_oak_leaves',
+	'minecraft:acacia_log': 'minecraft:acacia_leaves',
+	'minecraft:mangrove_log': 'minecraft:mangrove_leaves',
+	'minecraft:cherry_log': 'minecraft:cherry_leaves',
+	'minecraft:pale_oak_log': 'minecraft:pale_oak_leaves',
+	'minecraft:crimson_stem': 'minecraft:nether_wart_block',
+	'minecraft:warped_stem': 'minecraft:warped_wart_block',
+}
+const offsets = []
+for (let y = 0; y <= 1; y++) {
+	for (let x = -1; x <= 1; x++) {
+		for (let z = -1; z <= 1; z++) {
+			if (`${x}${y}${z}` == '000') continue
+			offsets.push({x, y, z})
+		}
+	}
+}
+
+// function search_for_leaves(block, type) {
+// 	const leaf_types = typeof logs[type] == 'string' ? [logs[type]] : logs[type]
+//     for (const x of [-1, 0, 1]) {
+//         for (const y of [-1, 0, 1]) {
+//             for (const z of [-1, 0, 1]) {
+//                 if (leaf_types.includes(block.offset({x, y, z}).typeId)) return true
+//             }
+//         }
+//     }
+// 	const log = search_for_log(block, type, new Set())
+// 	if (log.x == block.x && log.y == block.y && log.z == block.z) return
+// 	return search_for_leaves(log, type)
+// }
+
+function search_for_log(block, type, visited) {
+	const hash = `${block.x} ${block.y} ${block.z}`
+	visited.add(hash)
+	for (const offset of offsets) {
+		const found_block = block.offset(offset)
+		if (found_block.typeId != type) continue
+        const found_hash = `${found_block.x} ${found_block.y} ${found_block.z}`
+		if (visited.has(found_hash)) continue
+		return search_for_log(found_block, type, visited)
+	} return block
+}
+
+world.afterEvents.playerBreakBlock.subscribe(({block, player, brokenBlockPermutation:permutation, itemStackBeforeBreak:item}) => {
+	if (!item) return
+	if (!item.getTags().includes('minecraft:is_axe')) return // using axe
+	const type = permutation.type.id
+	if (!Object.keys(logs).includes(type)) return // breaking logs
+	if (player.inputInfo.getButtonState("Sneak") == "Pressed") return // not sneaking
+	// if(!search_for_leaves(block, type)) return  // make sure it's a tree, not functional
+	const last_log = search_for_log(block, type, new Set()) // find the farthest log
+	if (last_log.x == block.x && last_log.y == block.y && last_log.z == block.z) return  // not the same block
+	last_log.setPermutation(block.permutation)  // remove the last log
+	block.setPermutation(permutation)  // replace the broken log
+}) 
--- skybedrock_bp/scripts/items/creative_tool.js
+++ skybedrock_bp/scripts/items/creative_tool.js
@@ -11,6 +11,7 @@ export default {
 		.show(player).then(({ canceled, selection }) => {
 			if (canceled) return
 			if (selection == 0) stick.setLore(["§r§ndelete mobs"])
+			if (selection == 1) stick.setLore(["§r§ndelete blocks"])
 			player.getComponent("equippable").setEquipment('Mainhand', stick)
 		})
 	}
@@ -28,3 +29,53 @@ world.beforeEvents.playerInteractWithEntity.subscribe(event => {
 		if (mode == "delete mobs") entity.remove()
 	})
 })
+
+const offsets = []
+    for (const x of [-1, 0, 1]) {
+        for (const y of [-1, 0, 1]) {
+            for (const z of [-1, 0, 1]) {
+			if (`${x}${y}${z}` == '000') continue
+			offsets.push({x, y, z})
+		}
+	}
+}
+
+function hash_to_location(hash) {
+	return ((([x, y, z]) => ({x: +x, y: +y, z: +z})))(hash.split(' '))
+}
+
+function location_to_hash({x, y, z}) {
+	return `${x} ${y} ${z}`
+}
+
+function find_connected_blocks(block, type) {
+	const limit = 512
+	const block_hash = location_to_hash(block)
+	const connected_blocks = new Set()
+	const check_list = [block]
+	const searched = new Set([block_hash])
+	while (check_list.length != 0) {
+		const current_block = check_list.shift()
+		const current_hash = location_to_hash(current_block)
+		if (current_hash != block_hash) connected_blocks.add(current_hash)
+		if (connected_blocks.size >= limit - 1) break
+		for (const offset of offsets) {
+			const found_block = current_block.offset(offset) 
+			const found_hash = location_to_hash(found_block)
+			if (searched.has(found_hash)) continue
+			searched.add(found_hash)
+			if (found_block.typeId != type) continue
+			check_list.push(found_block)
+		}
+	}
+	return Array.from(connected_blocks).map(hash => block.dimension.getBlock(hash_to_location(hash)))
+}
+
+world.afterEvents.playerBreakBlock.subscribe(({block, dimension, brokenBlockPermutation:permutation, itemStackBeforeBreak:item}) => {
+	if (item?.typeId != "yasser444:creative_tool") return
+	const mode = item.getLore()[0]?.replace('§r§n', '')
+	const type = permutation.type.id
+	if (mode == "delete blocks") {
+		find_connected_blocks(block, type).forEach(block => block.setType('air'))
+	}
+}) 
